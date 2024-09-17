00ecef443493b13fefb5362298f56b182b97106b
ahmdarjn <ahmadarjun347@gmail.com>
2024-09-17 13:51:52 +0700

    01 fungsi perkenalan

---
+++ 01 FUNGSI PERKENALAN.php
@@ -0,0 +1,16 @@
+
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum, ";
+  echo "Perkenalkan, nama kita Ahmad arjun trisula<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+// memanggilnya lagi
+perkenalan();
+echo "<p><strong><i>By :Ahmad arjun trisula</strong>";
+?>
